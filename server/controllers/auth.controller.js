8a6954825cb87c540fad110190e9d719e721b65e
BlueOrang3 <matealtunashvili@gmail.com>
2023-03-31 03:52:05 +0400

    feat(add): authentication and authorization layers
    The req.auth object is populated by express-jwt in requireSignin after
    authentication verification, while req.profile is populated by the userByID
    function in user.controller.js. We will add the hasAuthorization function to
    routes that require both authentication and authorization.

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from '../models/user.model';
 import jwt from 'jsonwebtoken';
-import expressJwt from 'express-jwt';
+import { expressjwt } from "express-jwt";
 import config from './../../config/config';
 
 const signin = async (req, res) => {
@@ -27,8 +27,27 @@ const signin = async (req, res) => {
   }
 };
 
-const signout = (req, res) => {};
-const requireSignin = 'temp';
-const hasAuthorization = (req, res) => {};
+const signout = (req, res) => {
+  res.clearCookie('t');
+  return res.status('200').json({
+    message: 'signed out',
+  });
+};
+
+const requireSignin = expressjwt({
+  secret: config.jwtSecret,
+  userProperty: 'auth',
+  algorithms: ['HS256'],
+});
+
+const hasAuthorization = (req, res, next) => {
+  const authorized = req.profile && req.auth && req.profile._id == req.auth._id;
+  if (!authorized) {
+    return res.status('403').json({
+      error: 'User is not authorized',
+    });
+  }
+  next();
+};
 
 export default { signin, signout, requireSignin, hasAuthorization };
