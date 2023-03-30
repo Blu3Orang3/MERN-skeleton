b0f7f86f542849ae364e9ee00b75e5369316ae70
BlueOrang3 <matealtunashvili@gmail.com>
2023-03-31 00:01:46 +0400

    feat(add): users model finalized

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import crypto from 'crypto';
+
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -24,6 +26,15 @@ const UserSchema = new mongoose.Schema({
   salt: String,
 });
 
+UserSchema.path('hashed_password').validate(function (v) {
+  if (this._password && this._password.length < 6) {
+    this.invalidate('password', 'Password must be at least 6 characters.');
+  }
+  if (this.isNew && !this._password) {
+    this.invalidate('password', 'Password is required');
+  }
+}, null);
+
 UserSchema.virtual('password')
   .set(function (password) {
     this._password = password;
@@ -33,5 +44,25 @@ UserSchema.virtual('password')
   .get(function () {
     return this._password;
   });
+
+UserSchema.methods = {
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+  encryptPassword: function (password) {
+    if (!password) return '';
+    try {
+      return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex');
+    } catch (err) {
+      return '';
+    }
+  },
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + '';
+  },
+};
 
 export default mongoose.model('User', UserSchema);
