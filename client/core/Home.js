c4217b41a36d079a90ba38b501b954e8f5259f5b
BlueOrang3 <matealtunashvili@gmail.com>
2023-04-01 20:57:14 +0400

    feat(theming): mui theming and stylesheet

--- client/core/Home.js
+++ client/core/Home.js
@@ -1,1 +1,36 @@
+import React from 'react';
+import { makeStyles } from '@mui/styles';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import unicornbikeImg from './../assets/images/unicornbike.jpg';
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    maxWidth: 600,
+    margin: 'auto',
+    marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(5),
+  },
+  title: {
+    padding: `${theme.spacing(3)}px ${theme.spacing(2.5)}px ${theme.spacing(
+      2
+    )}px`,
+    color: theme.palette.openTitle,
+  },
+  media: {
+    minHeight: 400,
+  },
+  credit: {
+    padding: 10,
+    textAlign: 'right',
+    backgroundColor: '#ededed',
+    borderBottom: '1px solid #d0d0d0',
+    '& a': {
+      color: '#3f4771',
+    },
+  },
+}));
+
 
