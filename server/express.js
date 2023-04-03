cf19492199e0ecdc63963c57f7b1cbb60b305637
BlueOrang3 <matealtunashvili@gmail.com>
2023-04-04 00:34:09 +0400

    feat(add): basic server side rendering

--- server/express.js
+++ server/express.js
@@ -9,15 +9,21 @@ import template from './../template';
 import devBundle from './devBundle';
 import userRoutes from './routes/user.routes';
 import authRoutes from './routes/auth.routes';
+//server-side rendering
+import React from 'react';
+import * as ReactDOMServer from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom/server';
+import MainRouter from './../client/MainRouter';
+import { ServerStyleSheets } from '@mui/styles';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './../client/theme';
+//end
 
+const CURRENT_WORKING_DIR = process.cwd();
 const app = express();
 
 // comment out before building
 devBundle.compile(app);
-
-const CURRENT_WORKING_DIR = process.cwd();
-
-app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
 
 // parse body params and attache them to req.body
 app.use(bodyParser.json());
@@ -29,12 +35,34 @@ app.use(helmet());
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors());
 
-//routes
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
+
+//mount routes
 app.use('/', userRoutes);
 app.use('/', authRoutes);
 
-app.get('/', (req, res) => {
-  res.status(200).send(template());
+app.get('*', (req, res) => {
+  const sheets = new ServerStyleSheets();
+  const context = {};
+  const markup = ReactDOMServer.renderToString(
+    sheets.collect(
+      <StaticRouter location={req.url} context={context}>
+        <ThemeProvider theme={theme}>
+          <MainRouter />
+        </ThemeProvider>
+      </StaticRouter>
+    )
+  );
+  if (context.url) {
+    return res.redirect(303, context.url);
+  }
+  const css = sheets.toString();
+  res.status(200).send(
+    template({
+      markup: markup,
+      css: css,
+    })
+  );
 });
 
 app.use((err, req, res, next) => {
