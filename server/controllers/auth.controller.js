1e96b81a0545272466d26481a2d85c544e409abb
BlueOrang3 <matealtunashvili@gmail.com>
2023-04-04 00:24:31 +0400

    feat(fix): changed old syntax

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -6,10 +6,10 @@ import config from './../../config/config';
 const signin = async (req, res) => {
   try {
     let user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status('401').json({ error: 'User not found' });
+    if (!user) return res.status(401).json({ error: 'User not found' });
     if (!user.authenticate(req.body.password)) {
       return res
-        .status('401')
+        .status(401)
         .send({ error: "Email and password don't match." });
     }
     const token = jwt.sign({ _id: user._id }, config.jwtSecret);
@@ -23,13 +23,13 @@ const signin = async (req, res) => {
       },
     });
   } catch (err) {
-    return res.status('401').json({ error: 'Could not sign in' });
+    return res.status(401).json({ error: 'Could not sign in' });
   }
 };
 
 const signout = (req, res) => {
   res.clearCookie('t');
-  return res.status('200').json({
+  return res.status(200).json({
     message: 'signed out',
   });
 };
@@ -43,7 +43,7 @@ const requireSignin = expressjwt({
 const hasAuthorization = (req, res, next) => {
   const authorized = req.profile && req.auth && req.profile._id == req.auth._id;
   if (!authorized) {
-    return res.status('403').json({
+    return res.status(403).json({
       error: 'User is not authorized',
     });
   }
